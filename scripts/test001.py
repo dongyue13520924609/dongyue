3e69c548547b7e0afdf052ea7d23e05296e2ac26
dongyue13520924609 <769404351@qq.com>
2018-05-27 20:23:26 +0800

    haha

--- scripts/test001.py
+++ scripts/test001.py
@@ -8,6 +8,7 @@ class Test_001:
 
     @allure.step(title="测试步骤002")
     @pytest.mark.parametrize("a",[1,3,5])
-    def test_(self,a):
-        assert a!=2
+    def test_bcd(self,a):
+        allure.attach("描述","我是测试步骤002")
+        assert a != 2
 
