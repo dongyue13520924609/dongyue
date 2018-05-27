71c6bc0b6e4667c3006bb7b4d8d7dfe93ade2f93
dongyue13520924609 <769404351@qq.com>
2018-05-27 20:32:06 +0800

    haha

--- scripts/test001.py
+++ scripts/test001.py
@@ -1,7 +1,7 @@
 import pytest,allure
 
 class Test_001:
-    @pytest.step(title="测试步骤001")
+    @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a",[1,2,3])
     def test_abc(self,a):
         assert a != 2
